f6f9bb76f52ea7cd13b86f97fc09c19daa6935ca
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-12-30 11:06:01 +0530

    updated;

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "../src/redux/Store";
 import persistStore from "redux-persist/es/persistStore";
 
 import UserRoute from "./route/userRoutes";
