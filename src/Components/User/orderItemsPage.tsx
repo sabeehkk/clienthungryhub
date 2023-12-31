31235010bb8030813917fbe9860622c70424a7cf
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-12-31 16:18:30 +0530

    fixed pagination

--- src/Components/User/orderItemsPage.tsx
+++ src/Components/User/orderItemsPage.tsx
@@ -1,33 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { Fragment, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-// import Swal from "sweetalert2";
-// import { toast } from "react-toastify";
 import { userAxios } from "../../axios/axios";
 import OrderTrack from "../../assets/orderTrack";
 import { useNavigate, useParams } from "react-router-dom";
-// import PAgination from "../../Components/pagination";
+import PAgination from "../../Components/pagination";
 import { SuccessMessage, SwalAlert } from "../../utils/util";
 import { BiSolidChat } from "react-icons/bi";
 
 function OrderItems() {
   let total = 0;
-  // let charges = 0;
-  // let discount = 0;
-  // let grandTotal = 0;
   const [orderItem, setOrderItem] = useState<any>();
   const [is_chage, setChange] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [itemData, setItemDta] = useState<any>({});
   const navigate = useNavigate();
   const { ordId } = useParams();
-  // const [currentPage, setCurrentPage] = useState<number>(1);
-  // const handlePageChange = (page:any) => {
-  //   setCurrentPage(page);
-  // };
-  // const itemsPerPage = 5;
-  // const startIndex = (currentPage - 1) * itemsPerPage;
-  // const endIndex = startIndex + itemsPerPage;
+
   const ChatIcon = () => {
     return <BiSolidChat style={{ fontSize: "20px "   }} />;
   };
@@ -187,7 +176,7 @@ function OrderItems() {
         </div>
       </div>
       <div className="float- mr-3 mt-3">
-        {/* <PAgination /> */}
+        <PAgination size={undefined} filterPagination={undefined} currentPage={undefined} totalPages={undefined} onPageChange={undefined} />
       </div>
     </div>
   );
