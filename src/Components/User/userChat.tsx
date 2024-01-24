3744f39e15427d3f3e8ad02e03af717811de29e2
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-01-24 09:20:57 +0530

    updated user chat issue

--- src/Components/User/userChat.tsx
+++ src/Components/User/userChat.tsx
@@ -45,8 +45,6 @@ function UserChat() {
           const items = response?.data?.orderItems;
         });
     }
-    
-  
   };
 
   return (
@@ -83,7 +81,7 @@ function UserChat() {
               <a className="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
                 <img
                   className="object-cover w-10 h-10 rounded-full"
-                  src={Userdetails.profilePicture}
+                  src={Userdetails.profilePicture||"https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                   alt="username"
                 />
                 <div className="w-full pb-2">
