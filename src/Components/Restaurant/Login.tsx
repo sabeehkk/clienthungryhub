5c11f7cc2276a5f1db7a05447c9a1787bd1f83da
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-08-04 11:10:04 +0530

    updation

--- src/Components/Restaurant/Login.tsx
+++ src/Components/Restaurant/Login.tsx
@@ -37,7 +37,7 @@ const handleSubmit = async (e: SyntheticEvent)=>{
 }
   return (
     <>
-    <h2 ><span>email :</span> pumpkin@gmail.com</h2>
+    <h2 ><span>email :</span> pumbkin@gmail.com</h2>
     <h2><span>Password :</span> Asdfghj1@*</h2>
     <div className=" min-h-screen  flex items-center justify-center bg-white ">
     <div className="bg-white p-6 rounded-lg shadow w-96 mb-24">
